0b7556c917fd61b1c69318b692f371a18a9b08f8
thiago619 <jurassicpark171@gmail.com>
2025-09-08 17:38:39 -0300

    feat:start backend

---
+++ backend/src/main/java/web/dio/lab/board_tarefa/BoardTarefaApplication.java
@@ -0,0 +1,13 @@
+package web.dio.lab.board_tarefa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardTarefaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/web/dio/lab/board_tarefa/BoardTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package web.dio.lab.board_tarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
